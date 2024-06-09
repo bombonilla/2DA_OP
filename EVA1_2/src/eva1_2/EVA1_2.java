2b6f237d98cb12f1852afd9e1e037b80382be8cf
bombonilla <160804872+bombonilla@users.noreply.github.com>
2024-06-09 15:19:18 -0600

    Add files via upload

---
+++ EVA1_2/src/eva1_2/EVA1_2.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_2;
+
+/**
+ *
+ * @author eduar
+ */
+public class EVA1_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+     int valor = 100;
+        Persona perso1 = new Persona();
+        System.out.println(perso1);
+        Vehiculo carro = new Vehiculo();
+        System.out.println(carro);
+                
+              
+    }
+    
+}
+class Persona{
+    
+}
+
+class vehiculo{
+    String marca;
+    int annio;
+    String modelo;
+
+}
