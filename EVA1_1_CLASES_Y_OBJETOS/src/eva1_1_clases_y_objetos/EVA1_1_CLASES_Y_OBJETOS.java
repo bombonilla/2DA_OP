8152ca23d74cd967e32f72e1aa8fff1d6618d127
bombonilla <160804872+bombonilla@users.noreply.github.com>
2024-06-09 15:27:20 -0600

    Add files via upload

---
+++ EVA1_1_CLASES_Y_OBJETOS/src/eva1_1_clases_y_objetos/EVA1_1_CLASES_Y_OBJETOS.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_1_clases_y_objetos;
+
+/**
+ *
+ * @author eduar
+ */
+public class EVA1_1_CLASES_Y_OBJETOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+   Persona perso1;
+        int valor;
+    
+    }
+    
+}
+
+class Persona{
+    
+   
+}
